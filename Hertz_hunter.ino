3b12aeac03bfa5d9ae98d752ef07ccad09ce34e7
Simon Eason <sie.eason@gmail.com>
2025-02-20 11:11:14 +0800

    Changed back to long-press of select

--- Hertz_hunter.ino
+++ Hertz_hunter.ino
@@ -1,33 +1,42 @@
 #include <U8g2lib.h>
 
 // Define non-standard (i.e. non-i2c and non-spi) pins
-#define BACK_BUTTON 0
 #define PREVIOUS_BUTTON 21
 #define SELECT_BUTTON 20
 #define NEXT_BUTTON 10
 
+// Number of ms to delay for debouncing buttons
+#define DEBOUNCE_DELAY 200
+
+// How long button has to be held to be long-pressed
+#define LONG_PRESS_DURATION 800
+
+// Used to handle long-pressing SELECT to go back
+unsigned long selectButtonPressTime = 0;
+bool selectButtonHeld = false;
+
 // "Wifi", 14x14px
-const unsigned char epd_bitmap_Wifi [] PROGMEM = {
-	0x00, 0x00, 0x00, 0x00, 0xf8, 0x07, 0x04, 0x08, 0x02, 0x10, 0xf1, 0x23, 0x08, 0x04, 0x04, 0x08, 
-	0xe0, 0x01, 0x10, 0x02, 0x00, 0x00, 0xc0, 0x00, 0xc0, 0x00, 0x00, 0x00
+const unsigned char epd_bitmap_Wifi[] PROGMEM = {
+  0x00, 0x00, 0x00, 0x00, 0xf8, 0x07, 0x04, 0x08, 0x02, 0x10, 0xf1, 0x23, 0x08, 0x04, 0x04, 0x08,
+  0xe0, 0x01, 0x10, 0x02, 0x00, 0x00, 0xc0, 0x00, 0xc0, 0x00, 0x00, 0x00
 };
 // "Settings", 14x14px
-const unsigned char epd_bitmap_Settings [] PROGMEM = {
-	0xc4, 0x08, 0x2a, 0x15, 0x31, 0x23, 0x02, 0x10, 0x04, 0x08, 0xc6, 0x18, 0xe1, 0x21, 0xe1, 0x21, 
-	0xc6, 0x18, 0x04, 0x08, 0x02, 0x10, 0x31, 0x23, 0x2a, 0x15, 0xc4, 0x08
+const unsigned char epd_bitmap_Settings[] PROGMEM = {
+  0xc4, 0x08, 0x2a, 0x15, 0x31, 0x23, 0x02, 0x10, 0x04, 0x08, 0xc6, 0x18, 0xe1, 0x21, 0xe1, 0x21,
+  0xc6, 0x18, 0x04, 0x08, 0x02, 0x10, 0x31, 0x23, 0x2a, 0x15, 0xc4, 0x08
 };
 // "About", 14x14px
-const unsigned char epd_bitmap_About [] PROGMEM = {
-	0xe0, 0x01, 0x18, 0x06, 0x04, 0x08, 0xc2, 0x10, 0xc2, 0x10, 0x01, 0x20, 0x01, 0x20, 0xc1, 0x20, 
-	0xc1, 0x20, 0xc2, 0x10, 0xc2, 0x10, 0x04, 0x08, 0x18, 0x06, 0xe0, 0x01
+const unsigned char epd_bitmap_About[] PROGMEM = {
+  0xe0, 0x01, 0x18, 0x06, 0x04, 0x08, 0xc2, 0x10, 0xc2, 0x10, 0x01, 0x20, 0x01, 0x20, 0xc1, 0x20,
+  0xc1, 0x20, 0xc2, 0x10, 0xc2, 0x10, 0x04, 0x08, 0x18, 0x06, 0xe0, 0x01
 };
 
 // Array of all bitmaps for convenience. (Total bytes used to store images in PROGMEM = 144)
 const int epd_bitmap_allArray_LEN = 3;
 const unsigned char* epd_bitmap_allArray[3] = {
   epd_bitmap_Wifi,
-	epd_bitmap_Settings,
-	epd_bitmap_About
+  epd_bitmap_Settings,
+  epd_bitmap_About
 };
 
 // Setup display
@@ -35,7 +44,7 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 // Menu state
 int menuIndex = 0;
-const char *menuItems[] = { "Scan", "Settings", "About" };
+const char* menuItems[] = { "Scan", "Settings", "About" };
 const int menuLength = sizeof(menuItems) / sizeof(menuItems[0]);
 
 void drawMenu() {
@@ -72,7 +81,6 @@ void setup() {
   Serial.begin(115200);
 
   // Setup pins
-  pinMode(BACK_BUTTON, INPUT_PULLDOWN);
   pinMode(PREVIOUS_BUTTON, INPUT_PULLDOWN);
   pinMode(SELECT_BUTTON, INPUT_PULLDOWN);
   pinMode(NEXT_BUTTON, INPUT_PULLDOWN);
@@ -88,20 +96,37 @@ void loop() {
   // Move between menu items
   if (digitalRead(NEXT_BUTTON) == HIGH) {
     menuIndex = (menuIndex + 1) % menuLength;
-    delay(200);  // Debounce
+    delay(DEBOUNCE_DELAY);  // Debounce
   } else if (digitalRead(PREVIOUS_BUTTON) == HIGH) {
     menuIndex = (menuIndex - 1 + menuLength) % menuLength;
-    delay(200);  // Debounce
+    delay(DEBOUNCE_DELAY);  // Debounce
   }
 
+  // Handle pressing and holding select button to go back
   if (digitalRead(SELECT_BUTTON) == HIGH) {
-    if (menuIndex == 0) {
-      Serial.println("Scan");
-    } else if (menuIndex == 1) {
-      Serial.println("Settings");
-    } else if (menuIndex == 2) {
-      Serial.println("About");
+    if (selectButtonPressTime == 0) {
+      // Button just pressed so record time
+      selectButtonPressTime = millis();
+    } else if (!selectButtonHeld && millis() - selectButtonPressTime > LONG_PRESS_DURATION) {
+      // If held for longer than threshold, register long press
+      Serial.println("Back");
+      selectButtonHeld = true;
     }
-    delay(200);
+    delay(DEBOUNCE_DELAY);
+  } else {
+    // If button was pressed but wasn't held then use as SELECT rather than BACK
+    if (selectButtonPressTime > 0 && !selectButtonHeld) {
+      if (menuIndex == 0) {
+        Serial.println("Scan");
+      } else if (menuIndex == 1) {
+        Serial.println("Settings");
+      } else if (menuIndex == 2) {
+        Serial.println("About");
+      }
+    }
+
+    // Reset when button released
+    selectButtonPressTime = 0;
+    selectButtonHeld = false;
   }
 }
