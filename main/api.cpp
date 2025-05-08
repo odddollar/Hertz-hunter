89b4fdaf996da1c02d0d10c7b95805a46874701d
Simon Eason <sie.eason@gmail.com>
2025-05-08 11:21:57 +0800

    Added api endpoint for getting settings values

--- main/api.cpp
+++ main/api.cpp
@@ -1,14 +1,33 @@
 #include "api.h"
 
 // Initialise api
-API::API(const char *s, const char *pwd, int *bV)
-  : wifiOn(false), ssid(s), password(pwd), batteryVoltage(bV), server(80) {
+API::API(const char *s, const char *pwd, int *bV, int (*sI)[3])
+  : wifiOn(false), ssid(s), password(pwd), batteryVoltage(bV), settingsIndices(sI), server(80) {
 
+  // Endpoint for getting battery voltage
   server.on("/api/battery", HTTP_GET, [this](AsyncWebServerRequest *request) {
     JsonDocument doc;
 
     // Convert int to float with one decimal place
     doc["voltage"] = static_cast<float>(*batteryVoltage) / 10.0;
+
+    String json;
+    serializeJson(doc, json);
+
+    request->send(200, "application/json", json);
+  });
+
+  // Endpoint for getting settings indices
+  // This doesn't return the actual settings value, just the index
+  // Scan interval settings { 5, 10, 20 }
+  // Buzzer settings { On, Off }
+  // Battery alarm settings { 3.6, 3.3, 3.0 }
+  server.on("/api/settings", HTTP_GET, [this](AsyncWebServerRequest *request) {
+    JsonDocument doc;
+
+    doc["scan_interval"] = (*settingsIndices)[0];
+    doc["buzzer"] = (*settingsIndices)[1];
+    doc["battery_alarm"] = (*settingsIndices)[2];
 
     String json;
     serializeJson(doc, json);
