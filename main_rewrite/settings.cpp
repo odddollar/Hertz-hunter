7ab9f6b38a7d22cfa245ffa6886ffc1568635426
Simon Eason <sie.eason@gmail.com>
2025-05-21 17:05:00 +0800

    Settings object to store settings state

---
+++ main_rewrite/settings.cpp
@@ -0,0 +1,23 @@
+#include "settings.h"
+
+Settings::Settings()
+  // Initialise to defaults
+  : scanIntervalIndex(DEFAULT_INDEX), scanInterval(DEFAULT_SCAN_INTERVAL),
+    buzzerIndex(DEFAULT_INDEX), buzzer(DEFAULT_BUZZER),
+    batteryAlarmIndex(DEFAULT_INDEX), batteryAlarm(DEFAULT_BATTERY_ALARM) {
+
+  // When interval index changes, update actual interval
+  scanIntervalIndex.onChange([this](int val) {
+    scanInterval.set(5 * pow(2, val));
+  });
+
+  // When buzzer index changes, update buzzer state
+  buzzerIndex.onChange([this](int val) {
+    buzzer.set(val == 0 ? true : false);
+  });
+
+  // When battery index changes, update alarm threshold
+  batteryAlarmIndex.onChange([this](int val) {
+    batteryAlarm = -3 * val + 36;
+  });
+}
