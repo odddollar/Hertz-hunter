a7e5ebd5cdbb37934b4e752c78b528d4b58ca98e
Simon Eason <sie.eason@gmail.com>
2025-05-22 22:05:59 +0800

    Fixed writing settings to memory

--- main_rewrite/settings.cpp
+++ main_rewrite/settings.cpp
@@ -8,52 +8,53 @@ Settings::Settings()
     lowCalibratedRssi(DEFAULT_LOW_CALIBRATED_RSSI), highCalibratedRssi(DEFAULT_HIGH_CALIBRATED_RSSI),
     initialReadDone(false) {
 
-  // Set preferences namespace
-  preferences.begin("settings", false);
-
   // When interval index changes, update actual interval
   scanIntervalIndex.onChange([this](int val) {
     scanInterval.set(5 * pow(2, val));
-    if (initialReadDone) saveSettingsStorage("scan_interval_index", val);
+    if (initialReadDone) saveSettingsStorage("s_i_index", val);
   });
 
   // When buzzer index changes, update buzzer state
   buzzerIndex.onChange([this](int val) {
     buzzer.set(val == 0 ? true : false);
-    if (initialReadDone) saveSettingsStorage("buzzer_index", val);
+    if (initialReadDone) saveSettingsStorage("b_index", val);
   });
 
   // When battery index changes, update alarm threshold
   batteryAlarmIndex.onChange([this](int val) {
     batteryAlarm.set(-3 * val + 36);
-    if (initialReadDone) saveSettingsStorage("battery_alarm_index", val);
+    if (initialReadDone) saveSettingsStorage("b_a_index", val);
   });
 
   // Write calibration to storage on change
   lowCalibratedRssi.onChange([this](int val) {
-    if (initialReadDone) saveSettingsStorage("low_calibrated_rssi", val);
+    if (initialReadDone) saveSettingsStorage("l_c_rssi", val);
   });
 
   // Write calibration to storage on change
   highCalibratedRssi.onChange([this](int val) {
-    if (initialReadDone) saveSettingsStorage("high_calibrated_rssi", val);
+    if (initialReadDone) saveSettingsStorage("h_c_rssi", val);
   });
 }
 
 // Save given value to given key
 void Settings::saveSettingsStorage(const char *key, int value) {
+  preferences.begin("settings", false);
   preferences.putInt(key, value);
+  preferences.end();
   Serial.printf("%s: %i\n", key, value);
 }
 
 
 // Load all settings from memory
 void Settings::loadSettingsStorage() {
-  scanIntervalIndex.set(preferences.getInt("scan_interval_index", DEFAULT_INDEX));
-  buzzerIndex.set(preferences.getInt("buzzer_index", DEFAULT_INDEX));
-  batteryAlarmIndex.set(preferences.getInt("battery_alarm_index", DEFAULT_INDEX));
-  lowCalibratedRssi.set(preferences.getInt("low_calibrated_rssi", DEFAULT_LOW_CALIBRATED_RSSI));
-  highCalibratedRssi.set(preferences.getInt("high_calibrated_rssi", DEFAULT_HIGH_CALIBRATED_RSSI));
+  preferences.begin("settings", true);
+  scanIntervalIndex.set(preferences.getInt("s_i_index", DEFAULT_INDEX));
+  buzzerIndex.set(preferences.getInt("b_index", DEFAULT_INDEX));
+  batteryAlarmIndex.set(preferences.getInt("b_a_index", DEFAULT_INDEX));
+  lowCalibratedRssi.set(preferences.getInt("l_c_rssi", DEFAULT_LOW_CALIBRATED_RSSI));
+  highCalibratedRssi.set(preferences.getInt("h_c_rssi", DEFAULT_HIGH_CALIBRATED_RSSI));
+  preferences.end();
 
   // Used to prevent reading from non-volatile memory, updating variables, then immediately writing same value
   // Prevents unnecessary flash wear
@@ -62,6 +63,8 @@ void Settings::loadSettingsStorage() {
 
 // Clear everything and reset
 void Settings::clearReset() {
+  preferences.begin("settings", false);
   preferences.clear();
+  preferences.end();
   esp_restart();
 }
