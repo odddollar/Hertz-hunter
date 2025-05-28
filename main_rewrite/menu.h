bb457f6d25a8aeeb55ad2ab398413e87287a407f
Simon Eason <sie.eason@gmail.com>
2025-05-28 13:39:40 +0800

    Buzz on button press

--- main_rewrite/menu.h
+++ main_rewrite/menu.h
@@ -3,17 +3,21 @@
 
 #include <U8g2lib.h>
 #include "bitmaps.h"
+#include "buzzer.h"
 #include "RX5808.h"
 #include "settings.h"
 
 #define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGHT 64
 
+#define DEBOUNCE_DELAY 150
+
 // Holds menu state, and navigation and drawing functions
 class Menu {
 public:
-  Menu(uint8_t p_p, uint8_t s_p, uint8_t n_p, Settings *s);
+  Menu(uint8_t p_p, uint8_t s_p, uint8_t n_p, Settings *s, Buzzer *b);
   void begin();
+  void handleButtons();
   void drawBatteryVoltage(int voltage);
 
 private:
@@ -46,6 +50,7 @@ private:
   uint8_t select_pin;
   uint8_t next_pin;
   Settings *settings;
+  Buzzer *buzzer;
 
   // If using an OLED with an SH1106 chip then leave this be
   // If using an OLED with an SSD1306 chip then comment out the SH1106 line and uncomment the SSD1306 line
