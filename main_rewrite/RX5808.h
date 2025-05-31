21d9d3a2fc302854854a87dba8cba5b9545fab7e
Simon Eason <sie.eason@gmail.com>
2025-05-31 17:29:02 +0800

    Start of background scanning

--- main_rewrite/RX5808.h
+++ main_rewrite/RX5808.h
@@ -2,27 +2,39 @@
 #define RX5808_H
 
 #include <Arduino.h>
+#include "settings.h"
 
 #define MAX_FREQUENCIES_SCANNED 60 + 1
+#define MIN_FREQUENCY 5645
+#define SCAN_FREQUENCY_RANGE 300
 
 #define RSSI_STABILISATION_TIME 30
 #define RSSI_SAMPLES 30
 
+#define SCAN_STACK_SIZE 2048
+
 // RX5808 receiver module
 class RX5808 {
 public:
-  RX5808(uint8_t data, uint8_t le, uint8_t clk, uint8_t rssi);
+  RX5808(uint8_t data, uint8_t le, uint8_t clk, uint8_t rssi, Settings *s);
+  void startScan();
+  void stopScan();
+
+private:
+  static void _scan(void *parameter);
   void setFrequency(int frequency);
   int readRSSI();
-private:
   void reset();
   void sendRegister(byte address, unsigned long data);
   void sendBit(bool bit);
   unsigned long frequencyToRegister(int frequency);
+
   uint8_t dataPin;
   uint8_t lePin;
   uint8_t clkPin;
   uint8_t rssiPin;
+  TaskHandle_t scanHandle;
+  Settings *settings;
 };
 
 #endif
